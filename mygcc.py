c108c70856185ad0c94d6e1ec545dfac70818275
Jacek Wielemborek <d33tah@gmail.com>
2024-01-03 22:05:48 +0100

    mygcc: make it work, by using the placeholder

--- mygcc.py
+++ mygcc.py
@@ -13,10 +13,17 @@ if in_fname and out_fname:
     new_ofname = out_fname[0] + '.preprocessed'
     new_ifname = in_fname[0] + '.rewritten.c'
 
+    PLACEHOLDER = '/* d33tah */\n'
+
+    # add a placeholder and an include marker for each of the files
     with open(new_ifname, 'w') as f_out, open(in_fname[0], 'r') as f_in:
+        f_out.write(PLACEHOLDER)
         for n, line in enumerate(f_in.readlines()):
             if line.lstrip().startswith('#include'):
-                f_out.write(f'int include_starts_{n};\n')
+                # FIXME: if line contains the beginning of a comment,
+                # we can lose it. detect this and remove the comment
+                # the current workaround is to change those files manually.
+                f_out.write(f'int include_starts_{n}; // {line}')
                 f_out.write(line)
                 f_out.write(f'int include_ends_{n};\n')
             else:
@@ -31,11 +38,7 @@ if in_fname and out_fname:
         else:
             argv_rewritten.append(arg)
 
-    #subprocess.check_call(['gcc', '-E', '-P', '-dI'] + argv_rewritten)
     subprocess.check_call(['gcc', '-E', '-P', '-fdirectives-only', '-dI'] + argv_rewritten)
-
-    with open(new_ofname, 'r') as f_in:
-        lines = f_in.readlines()
 
     with open(in_fname[0]) as f:
         first_input_line = next(f)
@@ -45,13 +48,14 @@ if in_fname and out_fname:
         except StopIteration:
             has_second_line = False
 
+    with open(new_ofname, 'r') as f_in:
+        lines = f_in.readlines()
+
     if has_second_line:
-        #with open(in_fname[0], 'w') as f_out:
-        with open(new_ifname, 'w') as f_out:
+        with open(in_fname[0], 'w') as f_out:
             i = iter(lines)
-            while not line == first_input_line:
+            while not line == PLACEHOLDER:
                 line = next(i)
-            f_out.write(first_input_line)
             while True:
                 try:
                     line = next(i)
@@ -60,11 +64,10 @@ if in_fname and out_fname:
                 if line.startswith('int include_starts_'):
                     f_out.write(next(i))
                     while not line.startswith('int include_ends_'):
-                        #                    f_out.write(line + '//would be cut')
                         line = next(i)
                 else:
                     f_out.write(line)
 
-    #os.unlink(new_ifname)
+    os.unlink(new_ifname)
     os.unlink(new_ofname)
 os.execvp("gcc", ["gcc"] + sys.argv[1:])

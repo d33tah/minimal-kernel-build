359e7fbbc3502d2025b7bf11386f07a64f676bb6
d33tah <ghspam@d33.pl>
2024-11-18 02:58:51 +0100

    truncate include/linux/hrtimer_defs.h

--- minified/include/linux/hrtimer_defs.h
+++ minified/include/linux/hrtimer_defs.h
@@ -1,12 +1,1 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _LINUX_HRTIMER_DEFS_H
-#define _LINUX_HRTIMER_DEFS_H
 
-#include <linux/ktime.h>
-
-
-# define MONOTONIC_RES_NSEC	LOW_RES_NSEC
-# define KTIME_MONOTONIC_RES	KTIME_LOW_RES
-
-
-#endif

d98a76af3c55cd5990b50a82de98ff6b98d92469
d33tah <ghspam@d33.pl>
2024-11-18 04:03:25 +0100

    truncate include/linux/cfi.h

--- minified/include/linux/cfi.h
+++ minified/include/linux/cfi.h
@@ -1,17 +1,1 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * Clang Control Flow Integrity (CFI) support.
- *
- * Copyright (C) 2021 Google LLC
- */
-#ifndef _LINUX_CFI_H
-#define _LINUX_CFI_H
 
-
-
-
-#ifndef __CFI_ADDRESSABLE
-#define __CFI_ADDRESSABLE(fn, __attr)
-#endif
-
-#endif /* _LINUX_CFI_H */

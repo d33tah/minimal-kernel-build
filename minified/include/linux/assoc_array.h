1d91698ce771d517c462fb25e66468c434f8bf46
d33tah <ghspam@d33.pl>
2024-11-18 03:12:41 +0100

    truncate include/linux/assoc_array.h

--- minified/include/linux/assoc_array.h
+++ minified/include/linux/assoc_array.h
@@ -1,13 +1,1 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-/* Generic associative array implementation.
- *
- * See Documentation/core-api/assoc_array.rst for information.
- *
- * Copyright (C) 2013 Red Hat, Inc. All Rights Reserved.
- * Written by David Howells (dhowells@redhat.com)
- */
 
-#ifndef _LINUX_ASSOC_ARRAY_H
-#define _LINUX_ASSOC_ARRAY_H
-
-#endif /* _LINUX_ASSOC_ARRAY_H */

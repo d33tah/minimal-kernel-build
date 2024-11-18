7535c4230e956ba5ed66fb398914cb6abb56f7cc
d33tah <ghspam@d33.pl>
2024-11-18 04:36:27 +0100

    truncate include/net/netns/ieee802154_6lowpan.h

--- minified/include/net/netns/ieee802154_6lowpan.h
+++ minified/include/net/netns/ieee802154_6lowpan.h
@@ -1,19 +1,1 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * ieee802154 6lowpan in net namespaces
- */
 
-#include <net/inet_frag.h>
-
-#ifndef __NETNS_IEEE802154_6LOWPAN_H__
-#define __NETNS_IEEE802154_6LOWPAN_H__
-
-struct netns_sysctl_lowpan {
-};
-
-struct netns_ieee802154_lowpan {
-	struct netns_sysctl_lowpan sysctl;
-	struct fqdir		*fqdir;
-};
-
-#endif

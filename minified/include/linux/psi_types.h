ceffa4aab48f8f5c57ec6a6e89d36b54af4a7191
d33tah <ghspam@d33.pl>
2024-11-18 03:48:50 +0100

    truncate include/linux/psi_types.h

--- minified/include/linux/psi_types.h
+++ minified/include/linux/psi_types.h
@@ -1,15 +1,1 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _LINUX_PSI_TYPES_H
-#define _LINUX_PSI_TYPES_H
 
-#include <linux/kthread.h>
-#include <linux/seqlock.h>
-#include <linux/types.h>
-#include <linux/kref.h>
-#include <linux/wait.h>
-
-
-struct psi_group { };
-
-
-#endif /* _LINUX_PSI_TYPES_H */

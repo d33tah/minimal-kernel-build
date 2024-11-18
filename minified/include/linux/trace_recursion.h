338da8b290e60d53a3d76662b4ba038ccfba27f7
d33tah <ghspam@d33.pl>
2024-11-18 02:19:24 +0100

    truncate include/linux/trace_recursion.h

--- minified/include/linux/trace_recursion.h
+++ minified/include/linux/trace_recursion.h
@@ -1,8 +1,1 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _LINUX_TRACE_RECURSION_H
-#define _LINUX_TRACE_RECURSION_H
 
-#include <linux/interrupt.h>
-#include <linux/sched.h>
-
-#endif /* _LINUX_TRACE_RECURSION_H */

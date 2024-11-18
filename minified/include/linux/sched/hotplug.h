52b44c739f70b37ec6b263ca9887a84ca79014e6
d33tah <ghspam@d33.pl>
2024-11-18 04:23:28 +0100

    truncate include/linux/sched/hotplug.h

--- minified/include/linux/sched/hotplug.h
+++ minified/include/linux/sched/hotplug.h
@@ -1,18 +1,1 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _LINUX_SCHED_HOTPLUG_H
-#define _LINUX_SCHED_HOTPLUG_H
 
-/*
- * Scheduler interfaces for hotplug CPU support:
- */
-
-extern int sched_cpu_starting(unsigned int cpu);
-extern int sched_cpu_activate(unsigned int cpu);
-extern int sched_cpu_deactivate(unsigned int cpu);
-
-# define sched_cpu_wait_empty	NULL
-# define sched_cpu_dying	NULL
-
-static inline void idle_task_exit(void) {}
-
-#endif /* _LINUX_SCHED_HOTPLUG_H */
